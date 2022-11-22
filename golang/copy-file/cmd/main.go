99ea8dd2ff51d2cce4b8063a6b1846f7bc6de2e8
Gannochenko <gannochenko.sv@gmail.com>
2022-11-22 23:42:12 +0100

    done

--- golang/copy-file/cmd/main.go
+++ golang/copy-file/cmd/main.go
@@ -3,28 +3,41 @@ package main
 import (
     "io"
     "net/http"
+    "net/url"
     "errors"
     "os"
     "fmt"
     "log"
+    "time"
+    "context"
+    "strings"
 
-//     "github.com/pborman/uuid"
-//     "cloud.google.com/go/storage"
+     "github.com/pborman/uuid"
+     "cloud.google.com/go/storage"
 )
 
 func main() {
     fileURL := os.Getenv("FILE_URL")
+    bucketName := os.Getenv("BUCKET_NAME")
+    objectPath := os.Getenv("OBJECT_PATH")
 
-    _, closeReader, err := downloadFile(fileURL)
+    fileName, err := extractFileName(fileURL)
     if err != nil {
-        os.Exit(1)
+        panic(err)
+    }
+
+    fileReader, closeReader, err := downloadFile(fileURL)
+    if err != nil {
+        panic(err)
     }
     defer func() {
        err := closeReader()
        if err != nil {
-           os.Exit(1)
+           panic(err)
        }
     }()
+
+    uploadFile(fileName, fileReader, bucketName, objectPath)
 
     log.Println("Done!")
 }
@@ -49,4 +62,46 @@ func downloadFile(url string) (reader io.ReadCloser, Close func() error, err err
     }
 
     return resp.Body, resp.Body.Close, nil
+}
+
+func uploadFile(fileName string, fileReader io.ReadCloser, bucketName string, objectPath string) error {
+    uploaderCtx := context.Background()
+
+    uploaderCtx, cancel := context.WithTimeout(uploaderCtx, time.Second*50)
+    defer cancel()
+
+    targetObjectPath := objectPath+"/"+uuid.New()+"-"+fileName
+    log.Println("Uploading to "+bucketName+"/"+targetObjectPath)
+
+    client, err := storage.NewClient(uploaderCtx)
+    if err != nil {
+        return err
+    }
+
+    object := client.Bucket(bucketName).Object(targetObjectPath)
+    objectWriter := object.NewWriter(uploaderCtx)
+
+    if _, err := io.Copy(objectWriter, fileReader); err != nil {
+       return err
+    }
+    if err := objectWriter.Close(); err != nil {
+       return err
+    }
+
+    return nil
+}
+
+func extractFileName(fileURL string) (string, error) {
+    parsedURL, err := url.Parse(fileURL)
+    if err != nil {
+        return "", err
+    }
+
+    splitPath := strings.Split(parsedURL.Path, "/")
+
+    if len(splitPath) == 0 {
+        return "", nil
+    }
+
+    return splitPath[len(splitPath) - 1], nil
 }
