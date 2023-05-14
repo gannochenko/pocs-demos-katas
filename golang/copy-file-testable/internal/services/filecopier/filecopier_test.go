b0d361207c94865c8d1426e4898e8f7f875b037c
Gannochenko <gannochenko.sv@gmail.com>
2023-05-14 09:55:27 +0200

    wip

--- golang/copy-file-testable/internal/services/filecopier/filecopier_test.go
+++ golang/copy-file-testable/internal/services/filecopier/filecopier_test.go
@@ -13,45 +13,74 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
-type testFlow func(t *testing.T)
-
 func TestCopyFile(t *testing.T) {
+	createMocks := func() (*gcp.StorageClientMock, *ioMock.ReaderMock, *http.ClientMock) {
+		storageMock := gcp.NewStorageClientMock()
+		storageMock.On("UploadFile", mock.Anything, mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(nil)
+		storageMock.On("PrepareObjectPath", mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return("")
+
+		readerMock := ioMock.NewReaderMock()
+		readerMock.On("Read", mock.Anything).Return(0, io.EOF)
+		readerMock.On("Close").Return(nil)
+
+		httpClient := http.NewClientMock()
+		httpClient.On("ExtractFileName", mock.AnythingOfType("string")).Return("", nil)
+		httpClient.On("DownloadFile", mock.AnythingOfType("string")).Return(&internal.DocumentReadHandle{
+			Reader: readerMock,
+		}, nil)
+
+		return storageMock, readerMock, httpClient
+	}
+
+	type setup struct {
+		FileURL     string
+		BucketName  string
+		ObjectName  string
+		StorageMock *gcp.StorageClientMock
+		ReaderMock  *ioMock.ReaderMock
+		ClientMock  *http.ClientMock
+	}
+
+	type setupFunc func(t *testing.T) *setup
+	type verifyFunc func(t *testing.T, setup *setup, err error)
+
 	testCases := map[string]struct {
-		testFlow testFlow
+		setupFunc  setupFunc
+		verifyFunc verifyFunc
 	}{
-		"Should download a file": {
-			testFlow: func(t *testing.T) {
-				storageMock := gcp.NewStorageClientMock()
-				storageMock.On("UploadFile", mock.Anything, mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(nil)
-				storageMock.On("PrepareObjectPath", mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return("")
-
-				readerMock := ioMock.NewReaderMock()
-				readerMock.On("Read", mock.Anything).Return(0, io.EOF)
-				readerMock.On("Close").Return(nil)
-
-				httpClient := http.NewClientMock()
-				httpClient.On("ExtractFileName", mock.AnythingOfType("string")).Return("", nil)
-				httpClient.On("DownloadFile", mock.AnythingOfType("string")).Return(&internal.DocumentReadHandle{
-					Reader: readerMock,
-				}, nil)
-
-				fileCopier := filecopier.New(&filecopier.ServiceDependencies{
-					HttpClient:    httpClient,
-					StorageClient: storageMock,
-				})
-
-				err := fileCopier.CopyFile("http://path.to/file.pdf", "my-bucket", "my-object")
-
+		"Should return, unpaginated": {
+			setupFunc: func(t *testing.T) *setup {
+				storageMock, readerMock, httpClient := createMocks()
+				return &setup{
+					FileURL:     "http://path.to/file.pdf",
+					BucketName:  "my-bucket",
+					ObjectName:  "my-object",
+					StorageMock: storageMock,
+					ReaderMock:  readerMock,
+					ClientMock:  httpClient,
+				}
+			},
+			verifyFunc: func(t *testing.T, setup *setup, err error) {
 				assert.NoError(t, err)
-				httpClient.AssertCalled(t, "DownloadFile", mock.Anything)
-				storageMock.AssertCalled(t, "UploadFile", mock.Anything, mock.Anything, mock.Anything)
-				readerMock.AssertCalled(t, "Close")
+				setup.ClientMock.AssertCalled(t, "DownloadFile", mock.Anything)
+				setup.StorageMock.AssertCalled(t, "UploadFile", mock.Anything, mock.Anything, mock.Anything)
+				setup.ReaderMock.AssertCalled(t, "Close")
 			},
 		},
 	}
+
 	for name, tc := range testCases {
 		t.Run(name, func(t *testing.T) {
-			tc.testFlow(t)
+			setup := tc.setupFunc(t)
+
+			fileCopier := filecopier.New(&filecopier.ServiceDependencies{
+				HttpClient:    setup.ClientMock,
+				StorageClient: setup.StorageMock,
+			})
+
+			err := fileCopier.CopyFile(setup.FileURL, setup.BucketName, setup.ObjectName)
+
+			tc.verifyFunc(t, setup, err)
 		})
 	}
 }
