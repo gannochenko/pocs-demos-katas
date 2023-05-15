87b4eaa8dbf63be50efebb7309beba5e551ca450
Gannochenko <gannochenko.sv@gmail.com>
2023-05-15 23:06:06 +0200

    wip

--- golang/tree/cmd/main.go
+++ golang/tree/cmd/main.go
@@ -2,23 +2,24 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"tree/internal/lib/tree"
 )
 
 func main() {
-	//materialisedTree := map[string]int32{
-	//	"shop.foo.bar":    1,
-	//	"shop.foo.baz":    3,
-	//	"package.foo.bar": 7,
-	//	"package.foo.m":   10,
-	//}
+	materialisedTree := map[string]int32{
+		"shop.foo.bar":    1,
+		"shop.foo.baz":    3,
+		"package.foo.bar": 7,
+		"package.foo.m":   10,
+	}
 
 	treeInst := tree.New[int32]()
 
-	//for path, weight := range materialisedTree {
-	//	treeInst.AddNode(strings.Split(path, "."), weight)
-	//}
+	for path, weight := range materialisedTree {
+		treeInst.AddNode(strings.Split(path, "."), weight)
+	}
 
 	fmt.Println(treeInst.ToJSON())
 }
--- golang/tree/internal/lib/tree/tree.go
+++ golang/tree/internal/lib/tree/tree.go
@@ -1,6 +1,9 @@
 package tree
 
 import (
+	"encoding/json"
+	"strings"
+
 	"tree/internal/domain/tree"
 )
 
@@ -25,42 +28,38 @@ func New[P tree.PayloadType]() *Tree[P] {
 
 func (t *Tree[P]) AddNode(path []string, payload P) {
 	if len(path) > 0 {
-		//pathParts := make([]string, 0)
-		//
-		//for index, pathElement := range path {
-		//	pathParts = append(pathParts, pathElement)
-		//	parentPath := path[:len(pathParts)-1]
-		//	pathKey := strings.Join(pathParts, ".")
-		//	parentPathKey := strings.Join(parentPath, ".")
-		//
-		//	// if element was not created before
-		//	if _, ok := refs[pathKey]; !ok {
-		//		items := make([]*collection.Node, 0)
-		//		newNode := collection.Node{
-		//			Name: pathElement,
-		//			Item: &items,
-		//		}
-		//
-		//		if index == len(parseResult.Package.Path)-1 {
-		//			// adding a service here
-		//			for _, service := range parseResult.Package.Services {
-		//				itemsDeref := *newNode.Item
-		//				itemsDeref = append(itemsDeref, g.createServiceNode(service))
-		//				newNode.Item = &itemsDeref
-		//			}
-		//		}
-		//
-		//		toNode := refs[parentPathKey]
-		//		toNodeItemsDeref := *toNode.Item
-		//		toNodeItemsDeref = append(toNodeItemsDeref, &newNode)
-		//		toNode.Item = &toNodeItemsDeref
-		//
-		//		refs[pathKey] = &newNode
-		//	}
-		//}
+		pathParts := make([]string, 0)
+
+		for index, pathElement := range path {
+			pathParts = append(pathParts, pathElement)
+			parentPath := path[:len(pathParts)-1]
+			pathKey := strings.Join(pathParts, ".")
+			parentPathKey := strings.Join(parentPath, ".")
+
+			if _, ok := t.Refs[pathKey]; !ok {
+				items := make([]*tree.Node[P], 0)
+				newNode := tree.Node[P]{
+					Name:  pathElement,
+					Nodes: &items,
+				}
+
+				if index == len(path)-1 {
+					// adding payload
+					newNode.Payload = payload
+				}
+
+				toNode := t.Refs[parentPathKey]
+				toNodeNodesDeref := *toNode.Nodes
+				toNodeNodesDeref = append(toNodeNodesDeref, &newNode)
+				toNode.Nodes = &toNodeNodesDeref
+
+				t.Refs[pathKey] = &newNode
+			}
+		}
 	}
 }
 
 func (t *Tree[P]) ToJSON() string {
-	return ""
+	jsonData, _ := json.MarshalIndent(t.Root, "", "  ")
+	return string(jsonData)
 }
