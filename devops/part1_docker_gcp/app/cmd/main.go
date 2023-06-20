f7c4cdaf7cdfea528c11c290ecc6c685e269b3f2
Gannochenko <gannochenko.sv@gmail.com>
2023-06-20 20:17:51 +0200

    wip

--- devops/part1_docker_gcp/app/cmd/main.go
+++ devops/part1_docker_gcp/app/cmd/main.go
@@ -8,10 +8,18 @@ import (
 
 func main() {
 	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+        c.JSON(http.StatusOK, gin.H{
+            "hello": "there",
+        })
+    })
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	r.Run()
 }
