21f3f5184e3cff543c2a59e433b975e5b0179542
Sergei Gannochenko <s.gannochenko@c02c180mmd6t.ad.deliveryhero.com>
2024-02-12 23:05:57 +0100

    wip

--- golang/cycle/main.go
+++ golang/cycle/main.go
@@ -1,14 +1,41 @@
 package main
 
 import (
-    "context"
+	"context"
+	"fmt"
+	"sync"
+	"time"
 )
 
+const Interval = time.Second * 3
+
 func main() {
-    ctx := context.Background()
-    ctx, cancelCtx := context.WithCancel(ctx)
+	ctx := context.Background()
+	ctx, cancelCtx := context.WithCancel(ctx)
+	defer cancelCtx()
 
-    go func(){
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				fmt.Printf("context cancelled\n")
+				return
+			case <-time.After(Interval):
+				err := doStuff()
+				if err != nil {
+					cancelCtx()
+					return
+				}
+				fmt.Printf("tick!\n")
+			}
+		}
+	}()
 
-    }()
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	wg.Wait()
+}
+
+func doStuff() error {
+	return nil
 }
