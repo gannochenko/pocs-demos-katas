dae99814450865f003e9a34302e88f547c5042b2
Gannochenko <gannochenko.sv@gmail.com>
2024-03-04 19:09:39 +0100

    wip

--- devops/app/cmd/main.go
+++ devops/app/cmd/main.go
@@ -10,10 +10,10 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
-        c.JSON(http.StatusOK, gin.H{
-            "hello": "there",
-        })
-    })
+		c.JSON(http.StatusOK, gin.H{
+			"hello": "there1",
+		})
+	})
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
