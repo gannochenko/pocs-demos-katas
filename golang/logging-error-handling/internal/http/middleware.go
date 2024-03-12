55a21bc36547f52037928e1614859df68998f34d
Gannochenko <gannochenko.sv@gmail.com>
2024-03-12 12:57:55 +0100

    wip

--- golang/logging-error-handling/internal/http/middleware.go
+++ golang/logging-error-handling/internal/http/middleware.go
@@ -27,7 +27,7 @@ func ResponseWriter(controllerFn func(w http.ResponseWriter, r *http.Request) ([
 				httpStatus := http.StatusInternalServerError
 				err := syserr.Internal(fmt.Sprintf("PANIC: %v", rec))
 
-				writeError(httpStatus, err, w)
+				writeError(httpStatus, extractPublicMessage(syserr.InternalCode, err), w)
 				logRequest(r, err, httpStatus)
 			}
 		}()
@@ -37,9 +37,9 @@ func ResponseWriter(controllerFn func(w http.ResponseWriter, r *http.Request) ([
 		httpStatus := http.StatusOK
 
 		if err != nil {
-			httpStatus = mapErrorToHTTPStatus(err)
-
-			writeError(httpStatus, err, w)
+			errorCode := extractErrorCode(err)
+			httpStatus = mapErrorToHTTPStatus(errorCode, err)
+			writeError(httpStatus, extractPublicMessage(errorCode, err), w)
 		} else {
 			w.WriteHeader(httpStatus)
 			if len(responseBody) > 0 {
@@ -51,10 +51,11 @@ func ResponseWriter(controllerFn func(w http.ResponseWriter, r *http.Request) ([
 	})
 }
 
-func writeError(httpStatus int, err error, w http.ResponseWriter) {
+func writeError(httpStatus int, publicErrorMessage string, w http.ResponseWriter) {
 	w.WriteHeader(httpStatus)
+
 	responseBody, _ := json.Marshal(&ErrorResponse{
-		Error: err.Error(),
+		Error: publicErrorMessage,
 	})
 	_, _ = w.Write(responseBody)
 }
@@ -67,14 +68,15 @@ func logRequest(r *http.Request, err error, httpStatus int) {
 	fields[1] = logger.NewFiled("url", fmt.Sprintf("%s?%s", r.URL.Path, r.URL.RawQuery))
 	fields[2] = logger.NewFiled("status", httpStatus)
 
-	message := "request handled"
+	loggedMessage := "request handled"
 
 	// todo: we can also log the request body here, if needed
 
 	if err != nil {
-		message = fmt.Sprintf("%s with error: %s", message, err.Error())
+		errorCode := extractErrorCode(err)
 
-		loggerFn = mapErrorToLoggerFunction(err)
+		loggedMessage = fmt.Sprintf("%s with error: %s", loggedMessage, err.Error())
+		loggerFn = mapErrorCodeToLoggerFunction(errorCode)
 
 		stack := make([]string, 0)
 
@@ -93,43 +95,50 @@ func logRequest(r *http.Request, err error, httpStatus int) {
 		fields = append(fields, logger.NewFiled("stack", stack))
 	}
 
-	loggerFn(r.Context(), message, fields...)
+	loggerFn(r.Context(), loggedMessage, fields...)
 }
 
-func mapErrorToHTTPStatus(err error) int {
+func mapErrorToHTTPStatus(errorCode syserr.Code, err error) int {
+	switch errorCode {
+	case syserr.NotFoundCode:
+		return http.StatusNotFound
+	case syserr.BadInputCode:
+		return http.StatusBadRequest
+	default:
+		return http.StatusInternalServerError
+	}
+}
+
+func extractPublicMessage(errorCode syserr.Code, err error) string {
+	switch errorCode {
+	case syserr.InternalCode:
+		return "internal error occurred"
+	default:
+		return err.Error()
+	}
+}
+
+func mapErrorCodeToLoggerFunction(errorCode syserr.Code) func(ctx context.Context, message string, fields ...*logger.Field) {
+	switch errorCode {
+	case syserr.InternalCode:
+		return logger.Error
+	case syserr.NotFoundCode:
+		return logger.Warning
+	case syserr.BadInputCode:
+		return logger.Warning
+	default:
+		return logger.Error
+	}
+}
+
+func extractErrorCode(err error) syserr.Code {
 	var systemError *syserr.Error
 	ok := errors.As(err, &systemError)
 	if ok {
-		code := systemError.GetCode()
-
-		switch code {
-		case syserr.NotFoundCode:
-			return http.StatusNotFound
-		case syserr.BadInputCode:
-			return http.StatusBadRequest
-		}
+		return systemError.GetCode()
 	}
 
-	return http.StatusInternalServerError
-}
-
-func mapErrorToLoggerFunction(err error) func(ctx context.Context, message string, fields ...*logger.Field) {
-	var systemError *syserr.Error
-	ok := errors.As(err, &systemError)
-	if ok {
-		code := systemError.GetCode()
-
-		switch code {
-		case syserr.InternalCode:
-			return logger.Error
-		case syserr.NotFoundCode:
-			return logger.Warning
-		case syserr.BadInputCode:
-			return logger.Warning
-		}
-	}
-
-	return logger.Error
+	return syserr.InternalCode
 }
 
 func getErrorStackFormatted(e error) []string {
