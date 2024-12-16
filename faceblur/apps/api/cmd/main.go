57ece4bdffd9d135084252e216de9c5ca9a32e59
Sergei Gannochenko <s.gannochenko@c02c180mmd6t.ad.deliveryhero.com>
2024-12-16 15:59:11 +0100

    wip

--- faceblur/apps/api/cmd/main.go
+++ faceblur/apps/api/cmd/main.go
@@ -1,15 +1,45 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
-	"api/internal/foo"
-	imagepb "api/proto/image/v1"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"google.golang.org/grpc"
 )
 
 func main() {
-	r := imagepb.SubmitImageRequest{}
-	fmt.Printf("%v", r)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-	foo.Fn()
+	mux := runtime.NewServeMux()
+	var opts []grpc.DialOption
+	err := pb.RegisterYourServiceHandlerFromEndpoint(ctx, mux, grpcAddress, opts)
+	if err != nil {
+		log.Fatalf("Failed to register gRPC Gateway: %v", err)
+	}
+
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%d", conf.HTTPPort),
+		Handler: mux,
+		BaseContext: func(l net.Listener) context.Context {
+			address := l.Addr().String()
+			fmt.Println("Listening at " + address)
+			return ctx
+		},
+	}
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	<-sig
+
+	cancel()
+	_ = server.Shutdown(ctx)
 }
--- faceblur/apps/api/go.mod
+++ faceblur/apps/api/go.mod
@@ -1,10 +1,11 @@
 module api
 
-go 1.22
+go 1.22.7
 
-toolchain go1.22.0
+toolchain go1.22.10
 
 require (
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.24.0 // indirect
 	golang.org/x/net v0.31.0 // indirect
 	golang.org/x/sys v0.27.0 // indirect
 	golang.org/x/text v0.20.0 // indirect
@@ -13,4 +14,5 @@ require (
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20241209162323-e6fa225c2576 // indirect
 	google.golang.org/grpc v1.69.0 // indirect
 	google.golang.org/protobuf v1.35.2 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
