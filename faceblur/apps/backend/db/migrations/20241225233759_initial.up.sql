5c20210de615193f083f1270cb0845449cfa542a
Sergei Gannochenko <s.gannochenko@c02c180mmd6t.ad.deliveryhero.com>
2024-12-29 23:26:42 +0100

    wip

--- faceblur/apps/backend/db/migrations/20241225233759_initial.up.sql
+++ faceblur/apps/backend/db/migrations/20241225233759_initial.up.sql
@@ -1,10 +1,40 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+CREATE TABLE IF NOT EXISTS "users"
+(
+    "id"            uuid DEFAULT uuid_generate_v4(),
+    "sup"           varchar(255) NOT NULL,
+    "email"         varchar(255) NOT NULL,
+    "created_at"    timestamptz NOT NULL DEFAULT now(),
+    "updated_at"    timestamptz NOT NULL DEFAULT now(),
+    PRIMARY KEY ("id")
+);
+
 CREATE TABLE IF NOT EXISTS "images"
 (
-    "id"         uuid DEFAULT uuid_generate_v4(),
-    "created_at" timestamptz NOT NULL DEFAULT now(),
-    "updated_at" timestamptz NOT NULL DEFAULT now(),
-    "deleted_at" timestamptz,
-    PRIMARY KEY ("id")
+    "id"            uuid DEFAULT uuid_generate_v4(),
+    "url"           varchar(255),
+    "original_url"  varchar(255) NOT NULL,
+    "created_by"    uuid NOT NULL,
+    "created_at"    timestamptz NOT NULL DEFAULT now(),
+    "updated_at"    timestamptz NOT NULL DEFAULT now(),
+    "is_processed"  bool NOT NULL DEFAULT FALSE,
+    PRIMARY KEY ("id"),
+    FOREIGN KEY ("created_by") REFERENCES users("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS "image_processing_queue"
+(
+    "id"                uuid DEFAULT uuid_generate_v4(),
+    "image_id"          uuid NOT NULL,
+    "original_url"      varchar(255) NOT NULL,
+    "created_by"        uuid NOT NULL,
+    "created_at"        timestamptz NOT NULL DEFAULT now(),
+    "updated_at"        timestamptz NOT NULL DEFAULT now(),
+    "operation_id"      uuid NOT NULL,
+    "is_failed"         bool NOT NULL DEFAULT FALSE,
+    "failure_reason"    text,
+    PRIMARY KEY ("id"),
+    FOREIGN KEY ("created_by") REFERENCES users("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY ("image_id") REFERENCES images("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
