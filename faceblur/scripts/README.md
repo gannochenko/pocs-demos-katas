730f53e6cbbd552790b368edf7753181d8fa236a
Sergei Gannochenko <s.gannochenko@c02c180mmd6t.ad.deliveryhero.com>
2025-01-04 19:58:33 +0100

    wip

---
+++ faceblur/scripts/README.md
@@ -0,0 +1,18 @@
+# Running python scripts
+
+## Installations
+
+```
+python3 -m venv .venv
+
+source ./.venv/bin/activate
+
+pip3 install -r requirements.txt
+
+```
+
+## Usage
+
+```
+python bootstrap_gcp.py
+```
---
+++ faceblur/scripts/bootstrap_gcp.py
@@ -0,0 +1,13 @@
+import os
+from google.cloud import storage
+
+
+def create_bucket(bucket_name: str) -> None:
+    client = storage.Client()
+    bucket = client.bucket(bucket_name)
+    bucket.location = 'eu'
+    bucket.create()
+    print(f"Bucket created: {bucket.name}")
+
+
+create_bucket(os.getenv('DATA_EXPORT_BUCKET'))
