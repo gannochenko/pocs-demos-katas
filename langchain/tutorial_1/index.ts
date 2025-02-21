aa40515cecd8b7f46b635f5298c4c0d62a6935aa
Sergei Gannochenko <gannochenko.dev@gmail.com>
2025-02-22 00:11:01 +0100

    wip

---
+++ langchain/tutorial_1/index.ts
@@ -0,0 +1,13 @@
+import { ChatOpenAI } from "@langchain/openai";
+import { HumanMessage, SystemMessage } from "@langchain/core/messages";
+
+const model = new ChatOpenAI({ model: "gpt-4" });
+
+const messages = [
+  new SystemMessage("Translate the following from English into Italian"),
+  new HumanMessage("hi!"),
+];
+
+(async function name() {
+  await model.invoke(messages);
+})().catch(console.error);
