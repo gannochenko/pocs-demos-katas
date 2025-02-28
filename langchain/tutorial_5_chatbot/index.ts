bb16b765cb7e0b81d99d58c4b9802772b30edda5
Sergei Gannochenko <gannochenko.dev@gmail.com>
2025-02-28 16:01:17 +0100

    wip

--- langchain/tutorial_5_chatbot/index.ts
+++ langchain/tutorial_5_chatbot/index.ts
@@ -1,52 +1,71 @@
-import { ChatOpenAI, OpenAIEmbeddings } from '@langchain/openai';
-import { Document } from '@langchain/core/documents';
-import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf';
-import { RecursiveCharacterTextSplitter } from '@langchain/textsplitters';
-import { QdrantVectorStore } from '@langchain/qdrant';
+import { ChatOpenAI } from '@langchain/openai';
+import { START, END, MessagesAnnotation, StateGraph, MemorySaver, Annotation } from '@langchain/langgraph';
+import { v4 as uuidv4 } from 'uuid';
 import { ChatPromptTemplate } from '@langchain/core/prompts';
-import { z } from 'zod';
-import { StructuredOutputParser } from '@langchain/core/output_parsers';
+import { SystemMessage, HumanMessage, AIMessage, trimMessages } from '@langchain/core/messages';
 
 require('dotenv').config({ path: './.env.local' });
 
-const model = new ChatOpenAI({
+const llm = new ChatOpenAI({
   model: 'gpt-4o-mini-2024-07-18',
   temperature: 0,
 });
 
 (async function run() {
-  const personSchema = z.object({
-    name: z.optional(z.string()).describe('The name of the person'),
-    hair_color: z.optional(z.string()).describe("The color of the person's hair if known"),
-    height_in_meters: z.number().nullish().describe('Height measured in meters'),
+  const GraphAnnotation = Annotation.Root({
+    ...MessagesAnnotation.spec,
+    language: Annotation<string>(),
   });
 
-  const dataSchema = z.object({
-    people: z.array(personSchema).describe('Extracted data about people'),
+  const trimmer = trimMessages({
+    maxTokens: 10,
+    strategy: 'last',
+    tokenCounter: (msgs) => msgs.length,
+    includeSystem: true,
+    allowPartial: false,
+    startOn: 'human',
   });
 
   const promptTemplate = ChatPromptTemplate.fromMessages([
     [
       'system',
-      `You are an expert extraction algorithm.
-  Only extract relevant information from the text.
-  If you do not know the value of an attribute asked to extract,
-  return null for the attribute's value.`,
+      'You talk like a drunk Rick Sanchez. Answer all questions to the best of your ability in {language}.',
     ],
-    // Please see the how-to about improving performance with
-    // reference examples.
-    // ["placeholder", "{examples}"],
-    ['human', '{text}'],
+    ['placeholder', '{messages}'],
   ]);
 
-  const structuredModel = model.withStructuredOutput(dataSchema, {
-    name: 'person',
-  });
+  // Define the function that calls the model
+  const callModel = async (state: typeof GraphAnnotation.State) => {
+    const trimmedMessage = await trimmer.invoke(state.messages);
+    const prompt = await promptTemplate.invoke({ ...state, messages: trimmedMessage });
+    const response = await llm.invoke(prompt);
+    return { messages: response };
+  };
 
-  const prompt = await promptTemplate.invoke({
-    text: 'My name is Jeff, my hair is black and i am 6 feet tall. Anna has the same color hair as me.',
-  });
-  const result = await structuredModel.invoke(prompt);
+  // Define a new graph
+  const workflow = new StateGraph(GraphAnnotation)
+    // Define the node and edge
+    .addNode('model', callModel)
+    .addEdge(START, 'model')
+    .addEdge('model', END);
 
-  console.log(result);
+  // Add memory
+  const memory = new MemorySaver();
+  const app = workflow.compile({ checkpointer: memory });
+
+  const config = { configurable: { thread_id: uuidv4() } };
+
+  const output = await app.invoke(
+    {
+      messages: [
+        {
+          role: 'user',
+          content: 'Hi im bob',
+        },
+      ],
+      language: 'Russian',
+    },
+    config
+  );
+  console.log(output.messages[output.messages.length - 1].content);
 })().catch(console.error);
