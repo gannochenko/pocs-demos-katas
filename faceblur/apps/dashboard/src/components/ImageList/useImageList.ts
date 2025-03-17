3f7ca566c1125739e50da281a3e625d1ffdfb6f7
Sergei Gannochenko <gannochenko.dev@gmail.com>
2025-03-17 13:14:09 +0100

    wip

--- faceblur/apps/dashboard/src/components/ImageList/useImageList.ts
+++ faceblur/apps/dashboard/src/components/ImageList/useImageList.ts
@@ -1,148 +1,171 @@
-import {useEffect, useState} from "react";
-import {useAuth0} from "@auth0/auth0-react";
-import {PetListProps} from "./type";
-import {useListImages} from "@/hooks";
-import {ImageModel, Upload} from "@/models/image";
-import {isErrorResponse, uploadFile} from "@/util/fetch";
-import {useNotification} from "@/hooks/notification";
-import {useWebsocketContext} from "../WebsocketProvider";
-import {ServerMessage, ServerMessageType} from "@/proto/websocket/v1/websocket";
-import {GetUploadURL, SubmitImage} from "@/proto/image/v1/image";
+import { useEffect, useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { PetListProps } from "./type";
+import { LIST_IMAGES_KEY, useListImages } from "@/hooks";
+import { ImageModel, Upload } from "@/models/image";
+import { isErrorResponse, uploadFile } from "@/util/fetch";
+import { useNotification } from "@/hooks/notification";
+import { useWebsocketContext } from "../WebsocketProvider";
+import {
+  ServerMessage,
+  ServerMessageType,
+} from "@/proto/websocket/v1/websocket";
+import { GetUploadURL, SubmitImage } from "@/proto/image/v1/image";
+import { useQueryClient } from "react-query";
 
 const useUploader = () => {
-	const [uploads, setUploads] = useState<Upload[]>([]);
-	const { getAccessTokenSilently } = useAuth0();
-	const {showError} = useNotification();
-	const {addEventListener, removeEventListener} = useWebsocketContext();
+  const [uploads, setUploads] = useState<Upload[]>([]);
+  const { getAccessTokenSilently } = useAuth0();
+  const { showError } = useNotification();
 
-	useEffect(() => {
-		const handler = (payload: ServerMessage) => {
-			console.log("RECEIVED");
-			console.log(payload);
-		};
+  const updateUpload = (id: string, updateCb: (upload: Upload) => Upload) => {
+    setUploads((prevState) => {
+      return prevState.map((upload) => {
+        if (upload.id === id) {
+          return updateCb(upload);
+        }
 
-		addEventListener(ServerMessageType.SERVER_MESSAGE_TYPE_IMAGE_LIST, handler);
+        return upload;
+      });
+    });
+  };
 
-		return () => removeEventListener(ServerMessageType.SERVER_MESSAGE_TYPE_IMAGE_LIST, handler);
-	}, [addEventListener, removeEventListener]);
+  const showUploadError = (reason: string) => {
+    showError("Error uploading file", reason);
+  };
 
-	const updateUpload = (id: string, updateCb: (upload: Upload) => Upload) => {
-		setUploads(prevState => {
-			return prevState.map(upload => {
-				if (upload.id === id) {
-					return updateCb(upload);
-				}
+  return {
+    uploads,
+    submit: async (newUploads: Upload[]) => {
+      newUploads = newUploads.sort(
+        (a, b) =>
+          (b.uploadedAt?.getDate() ?? 0) - (a.uploadedAt?.getDate() ?? 0)
+      );
 
-				return upload;
-			});
-		});
-	};
+      setUploads((prevUploads) => {
+        return [...newUploads, ...prevUploads];
+      });
 
-	const showUploadError = (reason: string) => {
-		showError("Error uploading file", reason);
-	};
+      let token = "";
+      try {
+        token = await getAccessTokenSilently();
+      } catch (e) {
+        showError("Unauthorized");
+        return;
+      }
 
-	return {
-		uploads,
-		submit: async (newUploads: Upload[]) => {
-			newUploads = newUploads.sort((a, b) => (b.uploadedAt?.getDate() ?? 0) - (a.uploadedAt?.getDate() ?? 0));
-
-			setUploads(prevUploads => {
-				return [
-					...newUploads,
-					...prevUploads,
-				];
-			});
-
-			let token = "";
-			try {
-				token = await getAccessTokenSilently()
-			} catch (e) {
-				showError("Unauthorized");
-				return;
-			}
-
-			for (const upload of newUploads) {
-				const getUploadULRResponse = await GetUploadURL({}, token)
-				if (isErrorResponse(getUploadULRResponse)) {
-					showUploadError(getUploadULRResponse.error);
-					updateUpload(upload.id, (upload) => ({
-						...upload,
-						failed: true,
-					}))
-				} else {
-					await uploadFile(getUploadULRResponse.url, upload.file!, (newProgress) => {
-						updateUpload(upload.id, (upload) => ({
-							...upload,
-							progress: newProgress,
-						}))
-					});
-					const submitImageResponse = await SubmitImage({
-						image: {
-							objectName: getUploadULRResponse.objectName,
-							uploadedAt: upload.uploadedAt!,
-						},
-					}, token);
-					if (isErrorResponse(submitImageResponse)) {
-						showUploadError(submitImageResponse.error);
-						updateUpload(upload.id, (upload) => ({
-							...upload,
-							failed: true,
-						}))
-					} else {
-						updateUpload(upload.id, (upload) => ({
-							...upload,
-							image: submitImageResponse.image,
-						}))
-					}
-				}
-			}
-		},
-	};
+      for (const upload of newUploads) {
+        const getUploadULRResponse = await GetUploadURL({}, token);
+        if (isErrorResponse(getUploadULRResponse)) {
+          showUploadError(getUploadULRResponse.error);
+          updateUpload(upload.id, (upload) => ({
+            ...upload,
+            failed: true,
+          }));
+        } else {
+          await uploadFile(
+            getUploadULRResponse.url,
+            upload.file!,
+            (newProgress) => {
+              updateUpload(upload.id, (upload) => ({
+                ...upload,
+                progress: newProgress,
+              }));
+            }
+          );
+          const submitImageResponse = await SubmitImage(
+            {
+              image: {
+                objectName: getUploadULRResponse.objectName,
+                uploadedAt: upload.uploadedAt!,
+              },
+            },
+            token
+          );
+          if (isErrorResponse(submitImageResponse)) {
+            showUploadError(submitImageResponse.error);
+            updateUpload(upload.id, (upload) => ({
+              ...upload,
+              failed: true,
+            }));
+          } else {
+            updateUpload(upload.id, (upload) => ({
+              ...upload,
+              image: submitImageResponse.image,
+            }));
+          }
+        }
+      }
+    },
+  };
 };
 
 export function useImageList(props: PetListProps) {
-	const imagesResult = useListImages({pageNumber: 1});
-	const images: ImageModel[] = imagesResult.data?.images ?? [];
+  const queryClient = useQueryClient();
 
-	const {uploads, submit} = useUploader();
+  const imagesResult = useListImages({ pageNumber: 1 });
+  const images: ImageModel[] = imagesResult.data?.images ?? [];
 
-	// todo: reconcile images and uploads by id. if id is present in both, take the image, not the upload
+  const { addEventListener, removeEventListener } = useWebsocketContext();
 
-	return {
-		uploads,
-		images,
-		empty: !images.length && !uploads.length && !imagesResult.isLoading,
-		uploadButtonProps: {
-			onChange: async (files: File[]) => {
-				if (files.length) {
-					submit(files.map(file => (
-						{
-							id: Math.floor((Math.random() * 100000)).toString(),
-							file,
-							uploadedAt: new Date(),
-							progress: 0,
-						}
-					)))
-				}
-			}
-		},
-		getImageUploadProps: (upload: Upload) => {
-			return {
-				upload,
-			};
-		},
-		getImageUploadPropsByImage: (image: ImageModel) => {
-			return {
-				upload: {
-					id: image.id,
-					file: undefined,
-					image,
-					uploadedAt: image.updatedAt,
-					failed: image.isFailed,
-					progress: 100,
-				},
-			};
-		},
-	}
+  useEffect(() => {
+    const handler = (payload: ServerMessage) => {
+      console.log("RECEIVED");
+      console.log(payload);
+      // INVALIDATE!
+      queryClient.invalidateQueries([LIST_IMAGES_KEY]);
+    };
+
+    addEventListener(
+      ServerMessageType.SERVER_MESSAGE_TYPE_IMAGE_PROCESSED,
+      handler
+    );
+
+    return () =>
+      removeEventListener(
+        ServerMessageType.SERVER_MESSAGE_TYPE_IMAGE_PROCESSED,
+        handler
+      );
+  }, [addEventListener, removeEventListener]);
+
+  const { uploads, submit } = useUploader();
+
+  // todo: reconcile images and uploads by id. if id is present in both, take the image, not the upload
+
+  return {
+    uploads,
+    images,
+    empty: !images.length && !uploads.length && !imagesResult.isLoading,
+    uploadButtonProps: {
+      onChange: async (files: File[]) => {
+        if (files.length) {
+          submit(
+            files.map((file) => ({
+              id: Math.floor(Math.random() * 100000).toString(),
+              file,
+              uploadedAt: new Date(),
+              progress: 0,
+            }))
+          );
+        }
+      },
+    },
+    getImageUploadProps: (upload: Upload) => {
+      return {
+        upload,
+      };
+    },
+    getImageUploadPropsByImage: (image: ImageModel) => {
+      return {
+        upload: {
+          id: image.id,
+          file: undefined,
+          image,
+          uploadedAt: image.updatedAt,
+          failed: image.isFailed,
+          progress: 100,
+        },
+      };
+    },
+  };
 }
--- faceblur/apps/dashboard/src/components/WebsocketProvider/WebsocketProvider.ts
+++ faceblur/apps/dashboard/src/components/WebsocketProvider/WebsocketProvider.ts
@@ -1,116 +1,137 @@
-import useReactWebSocket, {ReadyState} from "react-use-websocket";
-import {getAPIURL} from "../../util/fetch";
-import {useAuth0} from "@auth0/auth0-react";
-import {ClientMessage, ClientMessageType, ServerMessage, ServerMessageType} from "../../proto/websocket/v1/websocket";
-import {createContext, createElement, PropsWithChildren, useContext, useEffect, useMemo, useRef} from "react";
+import useReactWebSocket, { ReadyState } from "react-use-websocket";
+import { getAPIURL } from "../../util/fetch";
+import { useAuth0 } from "@auth0/auth0-react";
+import {
+  ClientMessage,
+  ClientMessageType,
+  ServerMessage,
+  ServerMessageType,
+} from "../../proto/websocket/v1/websocket";
+import {
+  createContext,
+  createElement,
+  PropsWithChildren,
+  useContext,
+  useEffect,
+  useMemo,
+  useRef,
+} from "react";
 
 type WebsocketProviderProps = Record<string, unknown>;
 
 export type EventListener = (message: ServerMessage) => void;
-type AttachEventListener = (type: ServerMessageType, listener: EventListener) => void;
+type AttachEventListener = (
+  type: ServerMessageType,
+  listener: EventListener
+) => void;
 
 type ContextValue = {
-	addEventListener: AttachEventListener;
-	removeEventListener: AttachEventListener;
+  addEventListener: AttachEventListener;
+  removeEventListener: AttachEventListener;
 };
 
 const noop: AttachEventListener = () => {};
 
 const WebsocketContext = createContext<ContextValue>({
-	addEventListener: noop,
-	removeEventListener: noop,
+  addEventListener: noop,
+  removeEventListener: noop,
 });
 
 const makeMessage = <P>(type: ClientMessageType, payload: P): ClientMessage => {
-	return {
-		timestamp: new Date(),
-		type,
-		payloadVersion: "v1",
-		...payload,
-	};
+  return {
+    timestamp: new Date(),
+    type,
+    payloadVersion: "v1",
+    ...payload,
+  };
 };
 
 const makeTokenMessage = (token: string): ClientMessage => {
-	return makeMessage(ClientMessageType.CLIENT_MESSAGE_TYPE_TOKEN_UPDATE, {
-		tokenUpdate: {
-			token,
-		},
-	});
+  return makeMessage(ClientMessageType.CLIENT_MESSAGE_TYPE_TOKEN_UPDATE, {
+    tokenUpdate: {
+      token,
+    },
+  });
 };
 
-export const WebsocketProvider = ({ children }: PropsWithChildren<WebsocketProviderProps>) => {
-	const { getAccessTokenSilently } = useAuth0();
-	const url = `${getAPIURL()}/ws`;
+export const WebsocketProvider = ({
+  children,
+}: PropsWithChildren<WebsocketProviderProps>) => {
+  const { getAccessTokenSilently } = useAuth0();
+  const url = `${getAPIURL()}/ws`;
 
-	const { sendJsonMessage, lastJsonMessage, readyState } = useReactWebSocket(url, {
-		onOpen: async () => {
-			let token = "";
-			try {
-				token = await getAccessTokenSilently()
-			} catch (e) {
-				//showError("Unauthorized");
-				return;
-			}
+  const { sendJsonMessage, lastJsonMessage, readyState } = useReactWebSocket(
+    url,
+    {
+      onOpen: async () => {
+        let token = "";
+        try {
+          token = await getAccessTokenSilently();
+        } catch (e) {
+          //showError("Unauthorized");
+          return;
+        }
 
-			sendJsonMessage(makeTokenMessage(token));
-		},
-		retryOnError: true,
-		shouldReconnect: () => true,
-	});
+        sendJsonMessage(makeTokenMessage(token));
+      },
+      retryOnError: true,
+      shouldReconnect: () => true,
+    }
+  );
 
-	useEffect(() => {
-		const timer = setInterval(async () => {
-			if (readyState === ReadyState.OPEN) {
-				let token = "";
-				try {
-					token = await getAccessTokenSilently()
-				} catch (e) {
-					//showError("Unauthorized");
-					return;
-				}
+  useEffect(() => {
+    const timer = setInterval(async () => {
+      if (readyState === ReadyState.OPEN) {
+        let token = "";
+        try {
+          token = await getAccessTokenSilently();
+        } catch (e) {
+          //showError("Unauthorized");
+          return;
+        }
 
-				sendJsonMessage(makeTokenMessage(token));
-			}
-		}, 10000);
+        sendJsonMessage(makeTokenMessage(token));
+      }
+    }, 10000);
 
-		return () => clearInterval(timer);
-	}, [readyState, sendJsonMessage]);
+    return () => clearInterval(timer);
+  }, [readyState, sendJsonMessage]);
 
-	const listeners = useRef(new Map<ServerMessageType, EventListener[]>());
+  const listeners = useRef(new Map<ServerMessageType, EventListener[]>());
 
-	const value = useMemo<ContextValue>(() => {
-		return {
-			addEventListener: (type, cb) => {
-				if (!listeners.current.has(type)) {
-					listeners.current.set(type, []);
-				}
+  const value = useMemo<ContextValue>(() => {
+    return {
+      addEventListener: (type, cb) => {
+        if (!listeners.current.has(type)) {
+          listeners.current.set(type, []);
+        }
 
-				listeners.current.get(type)?.push(cb);
-			},
-			removeEventListener: (type, cb) => {
-				if (!listeners.current.has(type)) {
-					return;
-				}
+        listeners.current.get(type)?.push(cb);
+      },
+      removeEventListener: (type, cb) => {
+        if (!listeners.current.has(type)) {
+          return;
+        }
 
-				listeners.current.set(type, listeners.current.get(type)?.filter((savedCB) => savedCB !== cb) ?? []);
-			},
-		};
-	}, [listeners]);
+        listeners.current.set(
+          type,
+          listeners.current.get(type)?.filter((savedCB) => savedCB !== cb) ?? []
+        );
+      },
+    };
+  }, [listeners]);
 
-	useEffect(() => {
-		const message = lastJsonMessage as ServerMessage;
-		if (lastJsonMessage) {
-			listeners.current.get(message.type)?.forEach((listener) => {
-				listener(message);
-			});
-		}
-	}, [lastJsonMessage, listeners]);
+  useEffect(() => {
+    const message = lastJsonMessage as ServerMessage;
+    if (lastJsonMessage) {
+      const enumKey = message.type as unknown as keyof typeof ServerMessageType;
+      listeners.current.get(ServerMessageType[enumKey])?.forEach((listener) => {
+        listener(message);
+      });
+    }
+  }, [lastJsonMessage, listeners]);
 
-	return createElement(
-		WebsocketContext.Provider,
-		{ value: value },
-		children
-	);
+  return createElement(WebsocketContext.Provider, { value: value }, children);
 };
 
 export const useWebsocketContext = () => useContext(WebsocketContext);
